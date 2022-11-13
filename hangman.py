bd67b006cf86b335e8801dea723d103916ae7b83
Joseph J Burton <jburton378@gmail.com>
2022-11-13 09:05:44 -0700

    Updated some TODOs

--- hangman.py
+++ hangman.py
@@ -11,7 +11,7 @@ def clear():
         _ = system('cls')
 
 
-# TODO 1: future improvements - show a list of guessed letters
+# TODO 1: future improvements - show a list of guessed letters and number of lives remaining
 
 print(logo)
 game_is_finished = False
@@ -29,7 +29,7 @@ while not game_is_finished:
 
     # Use the clear() function imported to clear the output between guesses.
     clear()
-
+# TODO 4: It doesn't appear the clear function is working
     if guess in display:
         print(f"You've already guessed {guess}")
 
@@ -45,9 +45,11 @@ while not game_is_finished:
         if lives == 0:
             game_is_finished = True
             print("You lose.")
-
+# TODO 2: It may be nice to have what the word was at the end.
     if not "_" in display:
         game_is_finished = True
         print("You win.")
 
     print(stages[lives])
+
+# TODO 3: It would be good to add in an end message like "Game over"
